fb66f51c2d9ede235619c8333ac803f5952be81f
brayden1102 <eyssik05@naver.com>
2024-08-04 19:13:46 +0900

    chapter8 blog_web project

---
+++ 스터디_실습코드/김동환/chpater8/blog_web_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:blog_web_app/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // 플러터 프레임워크가 앱을 실행할 준비가 될 때까지 기다림
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
