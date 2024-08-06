5e1dddf1303b3717d097496c99e75426ff080f6a
goldenGlow21 <jonghoon8928@gmail.com>
2024-08-06 17:53:42 +0900

    4주차 실습코드 제출

---
+++ 스터디_실습코드/이종훈/ch9/flutter_workspace/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_workspace/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
