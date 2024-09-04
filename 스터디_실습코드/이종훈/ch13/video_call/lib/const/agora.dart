2f6317af79e059b985b3e85eb59689964f9b010c
goldenGlow21 <jonghoon8928@gmail.com>
2024-09-04 15:25:24 +0900

    8주차 실습코드 제출

---
+++ 스터디_실습코드/이종훈/ch13/video_call/lib/const/agora.dart
@@ -0,0 +1,3 @@
+const APP_ID = '106e4457d1da43c09f9e134ea0594255';
+const CHANNEL_NAME = 'testchannel';
+const TEMP_TOKEN = '007eJxTYLirb1HfZOkbd63PTWNu98uNvj9dXZqObKkPFFs+98xk01MKDIYGZqkmJqbmKYYpiSbGyQaWaZaphsYmqYkGppYmRqam75yvpzUEMjIYReQwMTJAIIjPzVCSWlySnJGYl5eaw8AAADogIi4=';
