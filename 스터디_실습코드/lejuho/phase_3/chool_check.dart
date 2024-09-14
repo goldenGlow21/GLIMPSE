30e8993c9c55361f43c3d9bd8c7e67a46dbb1cd3
lejuho <zedd1324@gmail.com>
2024-09-14 19:28:22 +0900

    docs : 챕터 14 업로드
    플러터 스터디 3단계 - 챕터 14 업로드

---
+++ 스터디_실습코드/lejuho/phase_3/chool_check.dart
@@ -0,0 +1,10 @@
+import '../phase_3/screen/chool_check_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
