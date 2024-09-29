4ae3c15f0367c296e2fb35cd0aa02e0628786628
brayden1102 <eyssik05@naver.com>
2024-09-29 22:28:48 +0900

    study week 16

---
+++ 스터디_실습코드/김동환/chapter16/cf_tube/lib/component/custom_youtube_player.dart
@@ -0,0 +1,68 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:cf_tube/model/video_model.dart';
+
+// 유튜브 재생기를 사용하기 위해 패키지 불러오기
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+// 유튜브 동영상 재생기가 될 위젯
+class CustomYoutubePlayer extends StatefulWidget {
+  // 상위 위젯에서 입력받을 동영상 정보
+  final VideoModel videoModel;
+
+  const CustomYoutubePlayer({
+    required this.videoModel,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CustomYoutubePlayer> createState() => _CustomYoutubePlayerState();
+}
+
+class _CustomYoutubePlayerState extends State<CustomYoutubePlayer> {
+  YoutubePlayerController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = YoutubePlayerController( // 1. 컨트롤러 선언
+        initialVideoId: widget.videoModel.id, // 처음 실행할 동영상 ID
+        flags: YoutubePlayerFlags(
+          autoPlay: false,  // 자동 실행 사용하지 않기
+        ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        YoutubePlayer(  // 2. 유튜브 재생기 렌더링
+            controller: controller!,
+            showVideoProgressIndicator: true,
+        ),
+        const SizedBox(height: 16.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            widget.videoModel.title,  // 동영상 제목
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        const SizedBox(height: 16.0,)
+      ],
+    );
+  }
+  @override
+  void dispose() {
+    super.dispose();
+
+    controller!.dispose();  // 3. State 폐기시 컨트롤러 또한 폐기
+  }
+}
---
+++ 스터디_실습코드/김동환/chapter16/cf_tube/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:cf_tube/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
