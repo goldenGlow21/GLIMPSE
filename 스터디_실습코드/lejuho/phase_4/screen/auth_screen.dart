70b1b256ae04da8ffea342209f2796b6e6a8a879
lejuho <zedd1324@gmail.com>
2024-12-20 18:00:00 +0900

    docs: 챕터 22 업로드(스터디 종료)

--- 스터디_실습코드/lejuho/phase_4/screen/auth_screen.dart
+++ 스터디_실습코드/lejuho/phase_4/screen/auth_screen.dart
@@ -3,179 +3,74 @@ import '../const/colors.dart';
 import '../database/drift_database.dart';
 import '../provider/schedule_provider.dart';
 import '../screen/calendar_scheduler_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class AuthScreen extends StatefulWidget {
+class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
   @override
-  State<AuthScreen> createState() => _AuthScreenState();
-}
-
-class _AuthScreenState extends State<AuthScreen> {
-  GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
-  String email = '';
-  String password = '';
-
-  @override
   Widget build(BuildContext context) {
-    final provider = context.watch<ScheduleProvider>();
-
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Align(
-                alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.7,
                 child: Image.asset(
                   'assets/img/logo.png',
-                  width: MediaQuery.of(context).size.width * 0.5,
                 ),
               ),
-              const SizedBox(height: 16.0),
-              LoginTextField(
-                onSaved: (val) {
-                  email = val!;
-                },
-                validator: (val) {
-                  if (val?.isEmpty ?? true) {
-                    return '이메일을 입력해주세요.';
-                  }
-
-                  RegExp reg = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-
-                  if (!reg.hasMatch(val!)) {
-                    return '이메일 형식이 올바르지 않습니다.';
-                  }
-                  return null;
-                },
-                hintText: '이메일',
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () => onGoogleLoginPress(context),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: SECONDARY_COLOR,
               ),
-              const SizedBox(height: 8.0),
-              LoginTextField(
-                onSaved: (val) {
-                  password = val!;
-                },
-                obscureText: true,
-                validator: (val) {
-                  if (val?.isEmpty ?? true) {
-                    return '비밀번호를 입력해주세요.';
-                  }
-
-                  if (val!.length < 4 || val.length > 8) {
-                    return '비밀번호는 4~8자 사이로 입력 해주세요!';
-                  }
-                  return null;
-                },
-                hintText: '비밀번호',
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: SECONDARY_COLOR,
-                ),
-                onPressed: () async {
-                  onRegisterPress(provider);
-                },
-                child: Text('회원가입'),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: SECONDARY_COLOR,
-                ),
-                onPressed: () async {
-                  onLoginPress(provider);
-                },
-                child: Text('로그인'),
-              ),
-            ],
-          ),
+              child: Text('구글로 로그인'),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  onRegisterPress(ScheduleProvider provider) async {
-    if (!saveAndValidateForm()) {
-      return;
-    }
-
-    String? message;
-
-    try {
-      await provider.register(
-        email: email,
-        password: password,
-      );
-    } on DioError catch (e) {
-      message = e.response?.data['message'] ?? '알 수 없는 오류가 발생했습니다.';
-    } catch (e) {
-      message = '알 수 없는 오류가 발생했습니다.';
-    } finally {
-      if (message != null) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(message),
-          ),
-        );
-      } else {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (_) => HomeScreen(),
-          ),
-        );
-      }
-    }
-  }
-
-  onLoginPress(ScheduleProvider provider) async {
-    if (!saveAndValidateForm()) {
-      return;
-    }
-
-    String? message;
+  onGoogleLoginPress(BuildContext context) async {
+    GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        'email',
+      ],
+    );
 
     try {
-      await provider.login(
-        email: email,
-        password: password,
+      GoogleSignInAccount? account = await googleSignIn.signIn();
+
+      final GoogleSignInAuthentication? googleAuth = await account?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
       );
-    } on DioError catch (e) {
-      message = e.response?.data['message'] ?? '알 수 없는 오류가 발생했습니다.';
-    } catch (e) {
-      message = '알 수 없는 오류가 발생했습니다.';
-    } finally {
-      if (message != null) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(message),
-          ),
-        );
-      } else {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (_) => HomeScreen(),
-          ),
-        );
-      }
+
+      final result = await FirebaseAuth.instance.signInWithCredential(credential);
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => HomeScreen(),
+        ),
+      );
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('로그인 실패')),
+      );
     }
-  }
-
-  bool saveAndValidateForm() {
-    if (!formKey.currentState!.validate()) {
-      return false;
-    }
-
-    formKey.currentState!.save();
-
-    return true;
   }
 }
